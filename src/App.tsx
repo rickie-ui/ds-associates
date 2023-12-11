cdda00ba1d979a368a44c56fc3c4a0457ee078d0
Erick Mucira <muciraerick254@gmail.com>
2023-12-11 23:21:54 +0300

    Initial commit - added home page, navigation  and updated page content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "@/pages/Layout";
+import Home from "@/pages/Home";
 
-const  App = () => {
-
+const App = () => {
   return (
-    <div className='text-secondary antialiased text-2xl'>
-       <p>hello</p>
+    <div className="text-2xl text-secondary antialiased">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
